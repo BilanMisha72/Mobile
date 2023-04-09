b1c72e8e72c4c9027fb55f48b4b1ba64ac88686d
Misha <bilan.md72@gmail.com>
2023-04-09 11:43:50 +0500

    change

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int remains = 200; //остаток
-        int payment = 1300;
+        int remainder = 200; //остаток
+        int payment = 1300; //платеж
         int percent = 0;
         if (payment > 1000) {
             percent = 1;
         }
         int bonus = (payment / 100) * percent;
 
-        int balance = (remains + payment + bonus);
+        int balance = (remainder + payment + bonus);
         System.out.println(balance);
     }
+
 }
-
-
